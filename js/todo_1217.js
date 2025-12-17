9c35c49cba20b4dc2701f6779dfd717ea5fc6490
kabus <fun@yedam.ac>
2025-12-17 17:41:54 +0900

    제어문(if)

--- js/todo_1217.js
+++ js/todo_1217.js
@@ -1,6 +1,6 @@
 // 15-2, 15-3장을 정독 (p499~p501까지)
 //git 설치
-// (D드라이브에)Dev/fun_javascript = gut clone git주소
+// (D드라이브에)Dev/fun_javascript = git clone git주소
 // 이름, 연락처, 주소 정보를 입력하면
 // 콘솔에 "이름은 000이고 전화번호는 000-000-0000이고 주소는 -----이다"
 // git add.
