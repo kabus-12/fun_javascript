5791edac6e62f9f1e50698c4dd1deab42cdbc52e
kabus <fun@yedam.ac>
2025-12-18 17:37:55 +0900

    18일 수업종료

--- js/function2.js
+++ js/function2.js
@@ -12,3 +12,50 @@ function sum(a , b){
 
 console.log(`전역 : ${num1}`);
 sum(1 , 2);
+num1 = 20;
+{
+  let num1 = 30;
+  console.log(num1);
+}
+console.log(num1);
+
+//코드
+//함수 : gugudan, 4단을 콘솔에 출력
+console.log('-------------------')
+function gugudan(dan){
+ for(i = 1; i <= 9; i++){
+    console.log(`${dan} * ${i} = ${dan * i}`);
+  }
+}
+gugudan(2);
+
+//p585 6번문제
+function sumMulti(a , b){
+  if(a === b){
+    num = a * b;
+  }
+  else{
+    num = a + b;
+  }
+  console.log(num);
+}
+ sumMulti(5 , 10);
+ sumMulti(10 , 10);
+ //p586 7번문제
+
+ let nu1 = prompt('비교할 첫번째 숫자 : ');
+ let nu2 = prompt('비교할 두번째 숫자 : ');
+
+ function nu(){
+  if(nu1 > nu2){
+    console.log(`${nu1}이(가) ${nu2}보다 큽니다.`);
+  }
+  else if(nu1 < nu2){
+    console.log(`${nu2}이(가) ${nu1}보다 큽니다.`);
+  }
+  else{
+    console.log(`두 수가 같습니다`)
+  }
+ }
+
+ nu(nu1 , nu2);
---
+++ todo/todo_1218.js
@@ -0,0 +1,15 @@
+//todo_1218.js
+
+//Math.random() 활용. 31 ~ 100까지.
+//5번 실행 (for문)
+//합계,평균(합계 / 실행수), 최대값 => "합계는 ---, 평균은 000, 최대값은 00"
+//함수 : scoreInfo
+
+// & 몇번 실행 (10 매개값으로 받아서 10번실행)
+//scoreInfo(10)
+
+//git add . 
+//git commit -m "18일 숙제완료"
+//git push -u origin main
+
+//시작할 때 git pull origin main 하기
