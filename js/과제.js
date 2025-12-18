b86f8c449d7ca0212a4d981e7ca2692adc0ade9c
kabus <fun@yedam.ac>
2025-12-18 10:25:39 +0900

    저장

---
+++ js/과제.js
@@ -0,0 +1,13 @@
+//과제.js
+
+let id = "안형주";
+let phone = "010-7349-4947";
+let address = "대구 수성구 파동로 120";
+
+console.log("이름은 " + id + "이고, 전화번호는 " + phone + "이고, 주소는 " + address + "입니다");
+
+let id2 = prompt("이름을 입력해주세요 : ");
+let phone2 = prompt("전화번호를 입력해주세요 : ");
+let address2 = prompt("주소를 입력해주세요 : ");
+
+console.log(`이름은 ${id2}이고, 전화번호는 ${phone2}이고, 주소는 ${address2}입니다.`);
