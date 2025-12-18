84d85874d63a63ddd47bad9c847bf6e431011dcc
kabus <fun@yedam.ac>
2025-12-18 10:28:44 +0900

    원격 푸시

--- js/todo_1217.js
+++ js/todo_1217.js
@@ -1,8 +1,13 @@
 // 15-2, 15-3장을 정독 (p499~p501까지)
-//git 설치
+// git 설치
 // (D드라이브에)Dev/fun_javascript = git clone git주소
 // 이름, 연락처, 주소 정보를 입력하면
 // 콘솔에 "이름은 000이고 전화번호는 000-000-0000이고 주소는 -----이다"
-// git add.
+
+// 로컬 -> 원격 동기화
+// git add .
 // git commit -m "12/17 숙제완료"
 // git push -u origin main
+
+// 원격 -> 로컬 동기화
+//git pull origin main (다른장소에서 푸쉬한 파일 들고오기)
